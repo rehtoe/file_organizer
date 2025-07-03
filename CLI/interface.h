8e3cf1355e2e762ebff954856183d8c187aac5f2
Ernesto <ejp1077@gmail.com>
2025-07-02 17:39:13 -0700

    testing multi commits, making interface

--- CLI/interface.h
+++ CLI/interface.h
@@ -1,9 +1,22 @@
 // interface header file
+#include <iostream>
+#include <string>
+
+enum class ArgumentOverload{
+    NONE = 0,
+    TARGET_DIRECTORY = 68, // 68, 100 = D, d
+    TARGET_LOCATION = 76, // 76, 108 = L, l
+    DEFAULT_LOCATION = 1,
+};
 
 struct Handler{
     public:
     Handler();
     Handler(int argc, char* argv[]);
     void help();
+    int error(ArgumentOverload argumentType);
     private:
+    int ERROR_usage();
+    int ERROR_sortDirectory();
+    int ERROR_locationDirectory();
 };
--- CLI/organize-cli.cpp
+++ CLI/organize-cli.cpp
@@ -1,7 +1,7 @@
 /*  using c++ for the cli due 
     gcc accessibility to build the files into executables
     */
-
+#include "interface.h"
 /*  Use case:
     CUSTOM LOCATION
     root_label -l "/a/b/"
@@ -20,15 +20,6 @@
 
     
     */
-
-#define ROOT_LABEL "sort-dir"
-
-enum class ArgumentOverload{
-    NONE = 0,
-    TARGET_DIRECTORY = 68, // 68, 100 = D, d
-    TARGET_LOCATION = 76, // 76, 108 = L, l
-    DEFAULT_LOCATION = 1,
-};
 
 int main(int argc, char* argv[]){
 
