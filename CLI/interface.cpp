97be231e2b0b87056090684f0ea468e6762d3d1c
Ernesto <ejp1077@gmail.com>
2025-07-02 17:46:58 -0700

    rewrote boilerplate, error functions not made. need logic for argument handler.

--- CLI/interface.cpp
+++ CLI/interface.cpp
@@ -1,2 +1,15 @@
 // interface source file
 #include "interface.h"
+
+Handler::Handler(){}
+Handler::Handler(int argc, char* argv[]){ for(int i=0;i<argc;i++){ arguments[i]=argv[i]; } ROOT_LABEL = arguments[0]; }
+void Handler::help(){
+    std::cout << "Command Usage Docs\n"
+            << "'" << ROOT_LABEL << "' is the leading command.\n"
+            << "'-D/-d  :   target directory to sort.'\n"
+            << "'-L/-l  :   location to move the files.'\n";
+}
+int Handler::error(ArgumentOverload argumentType){}
+int Handler::ERROR_usage(){}
+int Handler::ERROR_sortDirectory(){}
+int Handler::ERROR_locationDirectory(){}
--- CLI/interface.h
+++ CLI/interface.h
@@ -1,7 +1,9 @@
 // interface header file
 #include <iostream>
 #include <string>
+#include <map>
 
+// Argument overloads, enum = char(UPPERCASE)
 enum class ArgumentOverload{
     NONE = 0,
     TARGET_DIRECTORY = 68, // 68, 100 = D, d
@@ -9,8 +11,11 @@ enum class ArgumentOverload{
     DEFAULT_LOCATION = 1,
 };
 
+// Handler object
 struct Handler{
     public:
+    std::map<int, std::string> arguments;
+    std::string ROOT_LABEL;
     Handler();
     Handler(int argc, char* argv[]);
     void help();
